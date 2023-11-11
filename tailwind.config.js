f408fbeb616e6350b9c9e45d9d721db02969b62d
silencelamb <ooooooooooos@163.com>
2023-11-11 12:52:44 +0800

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,jsx,vue}', './src/*.{html,js,jsx,vue}'],
+  content: [
+    "./src/**/*.{html,js,jsx,vue}",
+    "./src/*.{html,js,jsx,vue}",
+    "./index.html",
+  ],
   theme: {
     extend: {},
   },
