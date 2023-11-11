bdcbd64db5beacd56d7a1e7a5a0998483c35403f
zywbsn <88329334+zywbsn@users.noreply.github.com>
2023-11-11 12:43:34 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,jsx,vue}', './src/*.{html,js,jsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
